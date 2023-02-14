9598050036b1cd52edfe0702a0b1b293934d27fb
Ichwan <ichwan.laruku@gmail.com>
2023-02-14 23:36:53 +0700

    Mapping Kelas 6

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge">s -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="node_modules/leaflet/dist/leaflet.css">
+    <link rel="stylesheet" href="assets/style.css">
+    <script src="node_modules/leaflet/dist/leaflet.js"></script>
+    <title>Mapping SD Anak Saleh</title>
+</head>
+
+<body>
+    <div id="map"></div>
+    <script src="assets/maps.js"></script>
+</body>
+
+</html>
